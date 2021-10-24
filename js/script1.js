d0af9cbc150a71684d084d85659c6c2a1d5cb460
duchuy.nguyen <duchuy.nguyen@edu.hefr.ch>
2021-10-24 23:20:48 +0200

    doc: sarah

--- js/script1.js
+++ js/script1.js
@@ -4,11 +4,16 @@ var images = [];
 var time = 5000;
 
 // image list
-images[0] = 'images/girl1.jpg';
-images[1] = 'images/girl2.jpg';
-images[2] = 'images/girl3.jpg';
-images[3] = 'images/girl4.jpg';
-images[4] = 'images/girl5.jpg';
+images[0] = 'images/1.jpg';
+images[1] = 'images/2.jpg';
+images[2] = 'images/3.jpg';
+images[3] = 'images/4.jpg';
+images[4] = 'images/5.jpg';
+images[5] = 'images/6.jpg';
+images[6] = 'images/7.jpg';
+images[7] = 'images/8.jpg';
+images[8] = 'images/9.jpg';
+images[9] = 'images/10.jpg';
 
 
 
